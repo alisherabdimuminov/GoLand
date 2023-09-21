50fa7b084fcb08dd3c91bfc7f3ffc84e95298a0d
alishercpp <alisher.abdimuminov.2005@gmail.com>
2023-09-21 19:08:25 +0500

    Go dasturlash tilida ozgaruvchilar

---
+++ lesson3.go
@@ -0,0 +1,17 @@
+// usr/bin/go
+// author: Ali
+// date  : 21/09/2023
+package main
+
+import "fmt"
+
+func main() {
+	// o'zgaruvchini e'lon qilish 1
+	// var var_name var_type = value
+	var name string = "Ali"
+	fmt.Println("My name is", name)
+
+	// o'zgaruvchini e'lon qilish 2
+	username := "@alishercpp"
+	fmt.Println("My telegram username is", username)
+}
